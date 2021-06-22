52cf8742dc7d44035a3f26bd97943a46bf6d108b
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-22 00:59:36 +0000

    debug assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  let message = "";
+  if (actual === expected) {
+    message = "✅✅✅Assertion Passed: " + actual + " === " + expected;
+  } else {
+    message = "🔴🔴🔴Assertion Passed: " + actual + " !== " + expected;
+  } return message;
+
+};
+
+// TEST CODE
+console.log(assertEqual(1,1));
