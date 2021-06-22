5dc94e0f11474069feb504b498953ebdb0e1ef4b
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-22 03:12:35 +0000

    fix some eslint issues

--- tail.js
+++ tail.js
@@ -17,12 +17,12 @@ let tail = function(array, expected) {
   let empty = array.slice(1);
 
   for (let i = 0; i < empty.length; i++) {
-    console.log(empty[i], expected[i])
+    console.log(empty[i], expected[i]);
     if (empty[i] !== expected[i]) {
-      return assertEqual(1, 2)
+      return assertEqual(1, 2);
     }
   }
-  return assertEqual(1, 1)
+  return assertEqual(1, 1);
 };
 
-console.log(tail(words, ["Lighthouse", "Labs"]))
+console.log(tail(words, ["Lighthouse", "Labs"]));
