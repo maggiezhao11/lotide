e78a457bb4b293f07e9275ebce31bf97e388c1c4
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-23 02:20:16 +0000

    fixing formatting

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,16 @@
 
-const eqArrays = function (array1, array2) {
-  for ( let i = 0; i < array1.length; i++) { 
-    if (array1[i]!== array2[i]) {
+const eqArrays = function(array1,array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       console.log([array1[i],array2[i]]);
-        return false;      
+      return false;
     }
   } return true;
 };
 
 const assertArraysEqual = function(actual, expected) {
   let message = "";
-  if (eqArrays(actual, expected)=== true) {
+  if (eqArrays(actual, expected) === true) {
     message = `✅✅✅Assertion Passed: ${actual} === ${expected}`;
   } else {
     message = `🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`;
@@ -18,4 +18,4 @@ const assertArraysEqual = function(actual, expected) {
 
 };
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); 
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
