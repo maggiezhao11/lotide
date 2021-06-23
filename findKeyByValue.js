15f7499012ce6c93ccfac4687f5930ff08112fe7
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-23 23:45:40 +0000

    after testing and formatting

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual,expected) {
+  let message = "";
+  if (actual === expected) {
+    message = `✅✅✅Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    message = `🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`;
+  } return message;
+
+};
+
+
+const findKeyByValue = function(object1,val1) {
+  for (let i of Object.keys(object1)) {
+    if (object1[i] === val1) {
+      return i;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
