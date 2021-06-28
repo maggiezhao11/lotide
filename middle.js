23e3e1c51679a4e24e6e31972ce3cab471deb68a
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-28 21:50:38 +0000

    refactor with module

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-const eqArrays = function(array1,array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      console.log([array1[i],array2[i]]);
-      return false;
-    }
-  } return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let message = "";
-  if (eqArrays(actual, expected)) {
-    message = `✅✅✅Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    message = `🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`;
-  } return message;
-
-};
-
-//console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-
 let middle = function(array) {
   let output = [];
   if (array.length <= 2) {
@@ -32,13 +8,15 @@ let middle = function(array) {
     output = [array[Math.floor((array.length - 1) / 2)] , array[Math.floor(array.length / 2)]];
   } return output;
 };
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1,2,3]));
-console.log(middle([1,2,3,4]));
-console.log(middle([1,2,3,5,6]));
 
-console.log(assertArraysEqual(middle([1]), []));
-console.log(assertArraysEqual(middle([1,2]), []));
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1, 2, 3,4]), [2,3]));
+module.exports = middle;
+// console.log(middle([1]));
+// console.log(middle([1, 2]));
+// console.log(middle([1,2,3]));
+// console.log(middle([1,2,3,4]));
+// console.log(middle([1,2,3,5,6]));
+
+// console.log(assertArraysEqual(middle([1]), []));
+// console.log(assertArraysEqual(middle([1,2]), []));
+// console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
+// console.log(assertArraysEqual(middle([1, 2, 3,4]), [2,3]));
