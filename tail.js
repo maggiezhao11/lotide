6c5fc98ebb2721773fa81e46f321e55fe89a6a65
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-28 22:42:28 +0000

    recode the file in order to meet mocha and chai requirement

--- tail.js
+++ tail.js
@@ -1,16 +1,10 @@
 const assertEqual = require('./assertEqual');
 
-let tail = function(array, expected) {
-  if (array.length >= 2) {
+let tail = function(array) {
+
     let empty = array.slice(1);
-    for (let i = 0; i < empty.length; i++) {
-      console.log(empty[i], expected[i]);
-      if (empty[i] !== expected[i]) {
-        return assertEqual(1, 2);
-      }
-    }
-  }
-  return assertEqual(1, 1);
+    
+  return empty;
 };
 
 //console.log(tail(words, ["Lighthouse", "Labs"]));
