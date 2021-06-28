176669066091562f706add4e200b02468a981579
maggiezhao11 <maggiezhao06@hotmail.com>
2021-06-28 22:44:42 +0000

    update test file for mocha and chai.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,18 @@
 
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
-const words = ["yo yo", "Lighthouse", "Labs"];
-console.log(tail(words, ["Lighthouse", "Labs"]));
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+// const words = ["yo yo", "Lighthouse", "Labs"];
+// console.log(tail(words, ["Lighthouse", "Labs"]));
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+});
